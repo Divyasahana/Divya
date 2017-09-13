e549dac1afa1ebfd6db7f63f3fb38fb71652b4cf
Divya.J <31921404+Divyasahana@users.noreply.github.com>
2017-09-13 16:24:02 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+clrscr();
+int n;
+printf("\nEnter a no");
+scanf(%d,&n);
+if(n==0)
+{
+printf("no is zero:);
+}
+else if(n%2==0)
+{
+printf("even no");
+}
+else
+{
+printf("odd no");
+}
+}
